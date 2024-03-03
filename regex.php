e08671639e632001349134d9f388fa900e69d192
Babyd12 <64022663+Babyd12@users.noreply.github.com>
2024-03-03 15:07:47 +0000

    Update regex.php
    ajoute de la regex faite depuis symony

--- regex.php
+++ regex.php
@@ -3,3 +3,6 @@
  $regex_date = '/^([0-2][0-9]|3[0-1])-([0-9]|1[0-2])-[0-9]{4}$/';
  $regexEmail = '/^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.%-]+\.[a-zA-Z]{2,}$/';
  $regex_phone_sn = '/^7[7-8-6-0-5]+[0-9]{7}$ /';
+
+
+ #[Assert\Regex("/^(?!\s*$)(?![0-9]+$)(?![^a-zA-Z0-9À-ÿ\s]+$)[a-zA-Z0-9À-ÿ'\s]*$/", message: "La valeur {{ value }}ne peut pas être vide ou composée uniquement d'espaces ou de caractères spéciaux")]
